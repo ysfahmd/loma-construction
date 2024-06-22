13ac77a9f1a9050fca1ae18c2d6d3e6dc8774dbf
Yusuf A <josephr98@gmail.com>
2024-06-22 09:31:24 -0400

    1) added styling for navbar, still have to add logo to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,11 +4,14 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <section>
-      <NavLink className='navbar-element' to='/' exact>Home</NavLink>
-      <NavLink className='navbar-element' to='/about' exact>About</NavLink>
-      <NavLink className='navbar-element' to='/services' exact>Services</NavLink>
+    <nav id='navbar'>
       <NavLink className='navbar-element' to='/contact-us' exact>Contact</NavLink>
-    </section>
+      <NavLink className='navbar-element' to='/services' exact>Services</NavLink>
+      <NavLink className='navbar-element' to='/about' exact>About</NavLink>
+      <NavLink className='navbar-element' to='/' exact>Home</NavLink>
+      <div id='logo'>
+        
+      </div>
+    </nav>
   )
 };
