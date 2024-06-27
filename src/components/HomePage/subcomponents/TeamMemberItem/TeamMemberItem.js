257ee3c06cace0f77a6e86e5260f19c7079538bd
Yusuf A <josephr98@gmail.com>
2024-06-26 23:41:53 -0400

    1) designed shell and structure for the 'meet-the-team-section' in the 'HomePage' component, missing colors

--- src/components/HomePage/subcomponents/TeamMemberItem/TeamMemberItem.js
+++ src/components/HomePage/subcomponents/TeamMemberItem/TeamMemberItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './TeamMemberItem.css';
+import headshotIcon from '../../../../assets/headshot-icon.jpg'
 
 export default function TeamMemberItem({ name, title, biography }) {
   return (
     <div id='team-member-item'>
+      <img src={headshotIcon} alt='temporary headshot placeholder until I get the actual pictures.' />
       <h3>{name}</h3>
       <h4>{title}</h4>
       <p>{biography}</p>
     </div>
   )
-}
+};
